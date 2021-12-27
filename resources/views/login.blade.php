a0586956d8384ae39304d0b5a22f4da2ccf80c7e
Parkash Suther <parkashsuther@gmail.com>
2021-12-27 11:14:33 +0500

    project added with basic setting

---
+++ resources/views/login.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>login here</h1>
+
+    <form method="POST">
+        <label for="">Enter URL</label>
+        <input type="text" placeholder="Shop Url" name="shop">
+        <button class="submit">Submit</button>
+    </form>
+</body>
+</html>
